ae1255fd44451dceb868edf62171c71f46e688f7
Reuben Brasher <rkbrasher@gmail.com>
2014-04-02 06:01:34 -0700

    Download data
    Download the Python pickled MNIST data from Yan LeCun's site.
    
    There is no need to uncompress these files, since Python is capable of doing that.

--- grabData.sh
+++ grabData.sh
@@ -5,43 +5,20 @@ function createDirectories()
     echo Creating data directories.
 
     mkdir data
-    mkdir data/mnist
-    mkdir data/cifar10
-    mkdir data/cifar100
+    mkdir data/cifarKaggle
+    mkdir data/cifarKaggle/test
+    mkdir data/cifarKaggle/train
 }
 
 function downloadFiles()
 {
     echo Downloading data.
 
-    curl http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz > data/cifar10/cifar-10-python.tar.gz
-    curl http://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz > data/cifar100/cifar-100-python.tar.gz
-    curl http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz > data/mnist/train-images-idx3-ubyte.gz
-    curl http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz > data/mnist/train-labels-idx1-ubyte.gz
-    curl http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz > data/mnist/t10k-images-idx3-ubyte.gz
-    curl http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz > data/mnist/t10k-labels-idx1-ubyte.gz
-}
-
-function unzipFiles()
-{
-    echo Unzipping data.
-
-    pushd .
-    cd data/cifar10/
-    gzip -dv < cifar-10-python.tar.gz | tar xvf - 
-    popd
-    pushd .
-    cd data/cifar100/
-    gzip -dv < cifar-100-python.tar.gz | tar xvf -
-    popd
-    
-    gzip -dv data/mnist/train-images-idx3-ubyte.gz
-    gzip -dv data/mnist/train-labels-idx1-ubyte.gz
-    gzip -dv data/mnist/t10k-images-idx3-ubyte.gz
-    gzip -dv data/mnist/t10k-labels-idx1-ubyte.gz
+    curl http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz > data/cifar-10-python.tar.gz
+    curl http://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz > data/cifar-100-python.tar.gz
+    curl http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz > data/mnist.pkl.gz
 }
 
 createDirectories
 downloadFiles
-unzipFiles
 
