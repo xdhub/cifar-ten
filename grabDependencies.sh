7bd2bc1cb6aa23a424ed2f762d360dd9ab8afffa
Reuben Brasher <rkbrasher@gmail.com>
2014-04-26 13:20:47 -0700

    Check whether files already downloaded

--- grabDependencies.sh
+++ grabDependencies.sh
@@ -14,9 +14,31 @@ function downloadFiles()
 {
     echo Downloading data.
 
-    curl http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz > data/cifar-10-python.tar.gz
-    curl http://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz > data/cifar-100-python.tar.gz
-    curl http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz > data/mnist.pkl.gz
+    pushd .
+    cd data
+
+    if [ -e mnist.pkl.gz ]
+    then
+        echo MNIST data set already downloaded.
+    else
+        curl http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz > mnist.pkl.gz
+    fi
+
+    if [ -e cifar-10-python.tar.gz ]
+    then
+        echo CIFAR 10 data set already downloaded.
+    else
+        curl http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz > cifar-10-python.tar.gz
+    fi
+    
+    if [ -e cifar-100-python.tar.gz ]
+    then
+        echo CIFAR 100 data set already downloaded.
+    else
+        curl http://www.cs.toronto.edu/~kriz/cifar-100-python.tar.gz > cifar-100-python.tar.gz
+    fi
+    
+    popd
 }
 
 function cloneRepos()
@@ -27,7 +49,14 @@ function cloneRepos()
     git clone git@github.com:lisa-lab/DeepLearningTutorials.git
     git clone git@github.com:fsprojects/Vulpes.git
     git clone git@github.com:jdeng/rbm-mnist.git
-    svn checkout http://cuda-convnet.googlecode.com/svn/trunk/ cuda-convnet-read-only
+    git clone git@github.com:nigma/pywt.git
+
+    if [ -e cuda-convnet-read-only ]
+    then
+        echo cuda-convnet repo set already cloned.
+    else
+        svn checkout http://cuda-convnet.googlecode.com/svn/trunk/ cuda-convnet-read-only
+    fi
     
     popd
 }
